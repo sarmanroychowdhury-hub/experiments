9da5ea9ef9967eb65afbbcd6190e067b2860b4c4
SARMAN ROY CHOWDHURY <sarmanroychowdhury@gmail.com>
2026-02-09 13:17:46 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,20 @@
+#base class
+class Vehicle:
+    def move(self):
+        print ("vehicle is moving")
+
+#derived class
+class Car(Vehicle):
+    def move(self):
+        print("Driving on the road")
+
+#derived class 2
+class Bicycle(Vehicle):
+    def move(self):
+        print("Pedaling on the road")
+
+#Demonstrating Polymorphism                
+vehicles = [Vehicle(), Car(), Bicycle()]
+
+for v in vehicles:
+    v.move()
