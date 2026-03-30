76ecb60cd85259f50299ffa1b8b044a561e97389
SARMAN ROY CHOWDHURY <sarmanroychowdhury@gmail.com>
2026-03-30 12:51:45 +0530

    Update and rename file_writing.py to EXP_file_writing.py

--- EXP_file_writing.py
+++ EXP_file_writing.py
@@ -1,17 +1,19 @@
 # Demonstration of File Handling Operations in Python
 # 1. Open a file in write mode and write data
-file = open ("deno_file.txt", "v")
+file = open ("demo_file.txt", "w")
 file.write("Hello! This is the first line in the file. \n") 
 file.write("Python file handling demonstration. \n") 
 file.close() #Properly closing the file
 print("Data written successfully. \n")
+
 # 2. Open the file in read mode and read contents
 file = open("demo_file.txt", "r")
 print ("Reading file contents:")
 content = file.read()
 print (content) 
 file.close()
-* 3. Open the file in append node and add more content 
+
+#3. Open the file in append node and add more content 
 file = open("demo_file.txt", "a")
 file.write("This line is added later using append mode. \n") 
 file.close()
